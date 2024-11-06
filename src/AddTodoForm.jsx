0d482e68e7de099ef2ebc9589b23721c5b994364
anelka777 <46974606+anelka777@users.noreply.github.com>
2024-11-05 17:24:02 -0800

    add new Todo List component and Add Todo Form

---
+++ src/AddTodoForm.jsx
@@ -0,0 +1,13 @@
+function AddTodoForm() {
+    return(
+        <div>
+            <form>
+                <label htmlFor="todoTitle">Title </label>
+                <input id="todoTitle"></input>
+                <button>Add</button>
+            </form>
+        </div>
+    )
+}
+
+export default AddTodoForm
--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import './App.css'
+import TodoList from './TodoList'
+import AddTodoForm from './AddTodoForm'
 
-const todoList = [
-  { id: 1, title: 'complete assignment' },
-  { id: 2, title: 'make a dinner' },
-  { id: 3, title: 'go shopping'},
-];
 
 function App() {
-
   return (
     <div>
       <h1>Todo List</h1>
-      <ul>
-        {todoList.map((todo) => (
-          <li key={todo.id}>{todo.title}</li>
-        ))}
-      </ul>
+      <AddTodoForm/>   
+      <TodoList/>
     </div>
   )
 }
