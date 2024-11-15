55bb386de9482a1617459e93cdbfa32968ec58f0
anelka777 <46974606+anelka777@users.noreply.github.com>
2024-11-15 08:19:45 -0800

    Refactor: Move list item to new component, handle form submit, and store new todo in state.

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
-// import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
+import { useState } from 'react'
 import './App.css'
 import TodoList from './TodoList'
 import AddTodoForm from './AddTodoForm'
 
 
 function App() {
+const [newTodo, setNewTodo] = useState('');
+
   return (
     <div>
       <h1>Todo List</h1>
-      <AddTodoForm/>   
+      <AddTodoForm onAddTodo={setNewTodo} />
+      <p>New Todo: {newTodo} </p>  
       <TodoList/>
     </div>
   )
